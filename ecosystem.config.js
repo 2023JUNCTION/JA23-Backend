0dd6067ae0d69a121d85be0543b4d39cb3822db0
Hosung Lee <bengaleehs@gmail.com>
2023-08-19 02:19:51 +0900

    pm2 setting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'app',
-            script: 'npm run start',
+            script: 'dist/main.js',
             instances: 0,
             exec_mode: 'cluster',
             wait_ready: false,
